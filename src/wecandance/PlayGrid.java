abfc1b41ccbe0d6014d2f30020e2223b1def29a4
1 <1@henhow>
2016-08-26 20:23:34 -0700

    Ensured resize yields all squares visible.

--- src/wecandance/PlayGrid.java
+++ src/wecandance/PlayGrid.java
@@ -1,9 +1,12 @@
 
 package wecandance;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.util.TimerTask;
+import java.util.Timer;
 import javax.swing.*;
 
 public class PlayGrid extends JPanel{
@@ -13,74 +16,99 @@ public class PlayGrid extends JPanel{
     private int yUnitSize;
     private int xDimensions;
     private int yDimensions;
+    private int xBorderSize;
+    private int yBorderSize;
     Square[][] squares;
-    
+    private boolean gameInSession;
+    java.util.Timer gameTime;
             
     PlayGrid(){
         this(500,500);
     }
-    
     PlayGrid(int xSize, int ySize){
         this(xSize,ySize, 100,100);
     }
-
     PlayGrid(int xSize, int ySize, int xDimensions, int yDimensions){
         this.xDimensions = xDimensions;
         this.yDimensions = yDimensions;
         squares = new Square[xDimensions][yDimensions];
         Resize(xSize, ySize);
+        //display any "get ready messages"
+        gameTime = new Timer();
     }
-  
+        TimerTask play = new TimerTask() {
+       
+        public void run(){
+            if(gameInSession){
+                for(int x = 0; x < xDimensions; x++){
+                    for(int y = 0; y < yDimensions; y++){
+                        if(squares[x][y].toString()== "Food")
+                            squares[x][y].setColour(Color.blue);
+                    }
+                }    
+            }
+        }
+        
+    };
     void Resize(int width, int height){
         xSize = width;
         ySize = height;
-        SetUnitSize();
+        xUnitSize = Math.floorDiv(xSize, xDimensions);
+        yUnitSize = Math.floorDiv(ySize, yDimensions);
+        System.out.printf("a square is %d by %d pixels \n", xUnitSize, yUnitSize);
         PopulateGrid();
+        
+        
     }
+    //add a fancy border around the playgrid...
+    void SteupBorder(){
+        xBorderSize = 0;
+        yBorderSize = 0;
+        
+    }
+    
     @Override
- public void paint (Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-                    g2.drawRect(100,100,xUnitSize,yUnitSize);
-                    for(int x = 0; x < xDimensions; x++){
+    public void paint (Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawRect(0,0,xSize+20,ySize+20);
+        for(int x = 0; x < xDimensions; x++){
             for(int y = 0; y < yDimensions; y++){
                 g2.setColor(squares[x][y].getColour());
-                    g2.draw(squares[x][y].getShape());
-                    g2.fill(squares[x][y].getShape());
-            }}
+                g2.draw(squares[x][y].getShape());
+                g2.fill(squares[x][y].getShape());
+            }
+        }
 }
-    /*
-    Determines the dimensions (in pixels) of a single square. 
-    */
-    private void SetUnitSize() {
-        setxUnitSize(getxSize() / getxDimensions());
-        setyUnitSize(getySize() / getyDimensions());
-        System.out.printf("a square is %d by %d pixels", getxUnitSize(), getyUnitSize());
-    }
+
     
     
     public void PopulateGrid(){
-        
-        for(int x = 0; x < xDimensions; x++){
-            for(int y = 0; y < yDimensions; y++){
-                int xloc = x*xUnitSize;
-                int yloc = y*yUnitSize;
-                
-                
-                
-                
-                squares[x][y] = (Math.random() <.5)? new Wall(xloc,yloc,xUnitSize,yUnitSize): new Food(xloc,yloc,xUnitSize,yUnitSize);  // this is a test
-               
+        if(! gameInSession){
+            for(int x = 0; x < xDimensions; x++){
+                for(int y = 0; y < yDimensions; y++){
+                    int xloc = x*xUnitSize;
+                    int yloc = y*yUnitSize;
+                    int random = (int)(Math.random()*100)+1;
+                    if(random <5)
+                        squares[x][y] = new Food(xloc,yloc,xUnitSize,yUnitSize);
+                    else if(random < 20)
+                        squares[x][y] = new Wall(xloc,yloc,xUnitSize,yUnitSize);
+                    else
+                        squares[x][y] = new Ground(xloc,yloc,xUnitSize,yUnitSize);
 
-
-                
-                
-            }
+                }
+            }// end of for loops
+            gameInSession = true;
         }
     }
     
     
+        
     
-    
+public void start(){
+                gameTime.schedule(play, 5000, 5000);
+
+}
     
     
     
@@ -184,6 +212,4 @@ public class PlayGrid extends JPanel{
         this.yDimensions = yDimensions;
     }
 
-
-    
 }
